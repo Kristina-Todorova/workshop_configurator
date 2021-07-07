6857c9cd7f12ab4dbac6bd0e70975b25625d1cf2
Teodora Kazakova <tk143@hdm-stuttgart.de>
2021-07-07 15:22:11 +0200

    ZF.js

--- src/Zusammenfassung.js
+++ src/Zusammenfassung.js
@@ -4,24 +4,20 @@ import Weiter from "./Weiter";
 import {Link} from "react-router-dom";
 import info_Icon from "./info_Icon.png";
 import "./Zusammenfassung.css";
-import GlobalDesignThinking from "./global/GlobalDesignThinking";
-import GlobalDesignSprint from "./global/GlobalDesignSprint";
-import GlobalLeanCanvas from "./global/GlobalLeanCanvas";
-import GlobalBusinessModel from "./global/GlobalBusinessModel";
-
-import GlobalTechTalk from "./global/GlobalTechTalk";
-import GlobalState from "./global/GlobalState";
-import GlobalCatering from "./global/GlobalCatering"
-import GlobalTechnik from "./global/GlobalTechnik";
-
 import GlobalInformative from "./global/GlobalInformative";
 import GlobalAuftrag from "./global/GlobalAuftrag";
 import GlobalErgebnis from "./global/GlobalErgebnis";
 import GlobalFort from "./global/GlobalFort";
 
-import GlobalDeepDive from "./global/GlobalDeepDive";
-import GlobalGruppenarbeit from "./global/GlobalGruppenarbeit"
-import GlobalBrainstorming from "./global/GlobalBrainstorming";
+import GlobalDeutsch from "./global/GlobalDeutsch";
+import GlobalEnglisch from "./global/GlobalEnglisch";
+import GlobalAndereSprache from "./global/GlobalAndereSprache";
+
+import GlobalPraesenz from "./global/GlobalPraesenz";
+import GlobalOnline from "./global/GlobalOnline";
+import GlobalAndereOrt from "./global/GlobalAndereOrt";
+import GlobalVon from "./global/GlobalVon";
+import GlobalBis from "./global/GlobalBis";
 
 import GlobalDigitalShift from "./global/GlobalDigitalShift";
 import GlobalFutureSales from "./global/GlobalFutureSales";
@@ -30,26 +26,86 @@ import GlobalFutureMobility from "./global/GlobalFutureMobility";
 import GlobalSustainability from "./global/GlobalSustainability";
 import GlobalAfterSales from "./global/GlobalAfterSales";
 
+import GlobalDesignThinking from "./global/GlobalDesignThinking";
+import GlobalDesignSprint from "./global/GlobalDesignSprint";
+import GlobalLeanCanvas from "./global/GlobalLeanCanvas";
+import GlobalBusinessModel from "./global/GlobalBusinessModel";
+import GlobalTechTalk from "./global/GlobalTechTalk";
+import GlobalPaketS from "./global/GlobalPaketS";
+import GlobalPaketM from "./global/GlobalPaketM";
+import GlobalPaketL from "./global/GlobalPaketL";
+import GlobalPrice from "./global/GlobalPrice";
+import GlobalTime from "./global/GlobalTime";
+import GlobalDeepDive from "./global/GlobalDeepDive";
+import GlobalGruppenarbeit from "./global/GlobalGruppenarbeit";
+import GlobalBrainstorming from "./global/GlobalBrainstorming";
+import GlobalCatering from "./global/GlobalCatering";
+import GlobalTechnik from "./global/GlobalTechnik";
+import GlobalState from "./global/GlobalState";
+import GlobalTeilnehmer from "./global/GlobalTeilnehmer";
+
 function Zusammenfassung(props) {
 
-    const ziel = props.ziel;
-    const fokus = props.fokus;
-    const methodik = props.methodik;
-    const allgemein = props.allgemein;
-    const umfang = props.umfang;
-    const grundbausteine = props.grundbausteine;
-    const konfiguration = props.konfiguration;
+    let [deutsch,setDeutsch] = useContext(GlobalDeutsch);
+    let [englisch,setEnglisch] = useContext(GlobalEnglisch);
+    let [anderesprache,setAndereSprache] = useContext(GlobalAndereSprache);
 
-    const catering = props.catering;
-    const technik = props.technik;
-    const movie = props.movie;
+    let [praesenz,setPraesenz] = useContext(GlobalPraesenz);
+    let [online,setOnline] = useContext(GlobalOnline);
+    let [andereort,setAndereOrt] = useContext(GlobalAndereOrt);
 
-    const deepdive = props.deepdive1;
-    const brainstorming = props.brainstorming1;
-    const gruppenarbeit = props.gruppenarbeit1;
+    let [teilnehmer,setTeilnehmer] = useContext(GlobalTeilnehmer);
+    let [von,setVon] = useContext(GlobalVon);
+    let [bis,setBis] = useContext(GlobalBis);
+
+    let [informative,setInformative] = useContext(GlobalInformative)
+    let [auftrag,setAuftrag] = useContext(GlobalAuftrag)
+    let [ergebnis,setErgebnis] = useContext(GlobalErgebnis)
+    let [fort,setFort] = useContext(GlobalFort)
+
+    let [digitalShift,setDigitalShift] = useContext(GlobalDigitalShift)
+    let [futureSales,setFutureSales] = useContext(GlobalFutureSales)
+    let [futureRetail,setFutureRetail] = useContext(GlobalFutureRetail)
+    let [futureMobility,setFutureMobility] = useContext(GlobalFutureMobility)
+    let [sustainability,setSustainability] = useContext(GlobalSustainability)
+    let [afterSales,setAfterSales] = useContext(GlobalAfterSales)
+
+    let [designthinking,setDesignThinking] = useContext(GlobalDesignThinking)
+    let [designsprint,setDesignSprint] = useContext(GlobalDesignSprint)
+    let [leancanvas,setLeanCanvas] = useContext(GlobalLeanCanvas)
+    let [businessmodel,setBusinessModel] = useContext(GlobalBusinessModel)
+    let [techtalk,setTechTalk] = useContext(GlobalTechTalk)
+
+    let [paketS,setPaketS] = useContext(GlobalPaketS)
+    let [paketM,setPaketM] = useContext(GlobalPaketM)
+    let [paketL,setPaketL] = useContext(GlobalPaketL)
+    let [pricecounter, setPriceCounter] = useContext(GlobalPrice)
+    let [timecounter, setTimeCounter] = useContext(GlobalTime)
+
+    let [deepdive, setDeepdive] = useContext(GlobalDeepDive)
+    let [gruppenarbeit, setGruppenarbeit] = useContext(GlobalGruppenarbeit)
+    let [brainstorming, setBrainstorming] = useContext(GlobalBrainstorming)
+
+    let [catering,setCatering] = useContext(GlobalCatering)
+    let [technik,setTechnik] = useContext(GlobalTechnik)
+    let [movie,setMovie] = useContext(GlobalState)
 
 
+    const ziel1 = props.ziel1;
+    const fokus1 = props.fokus1;
+    const methodik1 = props.methodik1;
+    const allgemein1 = props.allgemein1;
+    const umfang1 = props.umfang1;
+    const grundbausteine1 = props.grundbausteine1;
+    const konfiguration1 = props.konfiguration1;
 
+    const catering1 = props.catering1;
+    const technik1 = props.technik1;
+    const movie1 = props.movie1;
+
+    const deepdive1 = props.deepdive1;
+    const brainstorming1 = props.brainstorming1;
+    const gruppenarbeit1 = props.gruppenarbeit1;
 
 
 
@@ -88,9 +144,9 @@ function Zusammenfassung(props) {
 
                   <div className="zielworkshop">
                     <p>Ziel des Workshops:</p>
-                     {ziel
-                         ? <p>{props.ziel}</p>
-                         : <p className="leer">Kein Ziel ausgewaehlt</p>
+                     {ziel1
+                         ? <p>{props.ziel1}</p>
+                         : <p className="leer">Kein Ziel ausgewählt</p>
                          }
 
                    </div>
@@ -100,9 +156,15 @@ function Zusammenfassung(props) {
 
                   <div className="fokusthema">
                     <p>Fokusthema:</p>
+                    {fokus1
+                          && <p className="cartitem">{props.fokus1}</p>
+                          }
                     </div>
                   <div className="methodik">
                     <p>Methodik:</p>
+                    {methodik1
+                          && <p className="cartitem">{props.methodik1}</p>
+                          }
                     </div>
 
                     <div className="allginfo">
@@ -111,6 +173,9 @@ function Zusammenfassung(props) {
 
                     <div className="umfang">
                     <p>Umfang:</p>
+                    {umfang1
+                           && <p className="cartitem">{props.umfang1}</p>
+                           }
                     </div>
 
                     <div className="grundbausteine">
@@ -118,11 +183,22 @@ function Zusammenfassung(props) {
                     </div>
 
                     <div className="konfiguration">
-                    <p>Konfiguration>:</p>
+                    <p>Konfiguration:</p>
                     </div>
 
                     <div className="zusatzleistungen">
                     <p>Zusatzleistungen:</p>
+                    {brainstorming1
+                        && <p className="cartitem">{props.brainstorming1}</p>
+                        }
+
+                   {deepdive1
+                        && <p className="cartitem">{props.deepdive1}</p>
+                        }
+
+                   {gruppenarbeit1
+                        && <p className="cartitem">{props.gruppenarbeit1}</p>
+                        }
                     </div>
 
                     <div className="dauer">
